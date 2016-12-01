da28eb0cacadbd6a3fe73d287b4f9c054ff9ac01
Nick Steffen <nicksteffen94@gmail.com>
2016-11-30 20:04:41 -0500

    adding http post handling/sending to app

--- groupmebot.py
+++ groupmebot.py
@@ -1,13 +1,28 @@
 import os
 from flask import Flask
+from flask import request
+import requests
 
 
 app= Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 
 def hello(self):
     return "Hello World!"
+
+def response(self):
+    if request.method == 'POST':
+        data = request.form
+
+    return data['text']
+
+def send_post(self):
+    mybot_id= 0
+    msg = "Hello"
+    r = requests.post('https://api.groupme.com/v3/bots/post',data ={
+        'bot_id':'mybot_id', 'text':'msg'})
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
